51a57598b2444324c97b2cd655c8da353a7cef36
andriiholubenko <andrii@astrumu.com>
2023-07-14 12:47:08 +0300

    feat: ASU-2487 - Merge changesets into one file

--- src/index.ts
+++ src/index.ts
@@ -276,12 +276,12 @@ ${changeset.summary}`;
 
   if (changesets.releases.length) {
     const changesetContents = `
-  ---
-  ${changesets.releases
-    .map((release) => `'${release.name}': ${release.type}`)
-    .join("\n")}
-  ${changesets.summary}
-  `;
+---
+${changesets.releases
+  .map((release) => `'${release.name}': ${release.type}`)
+  .join("\n")}
+${changesets.summary}
+`;
 
     console.debug(`Writing changeset to ${filePath}`, changesetContents);
 
